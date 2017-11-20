6d9e07c7a67f7de2ff63e2ebd17c1347b20d1ab7
Pierrain Emmanuel <32103847+EmPierrain@users.noreply.github.com>
2017-11-20 16:15:55 +0100

    Ajout dictionnaire

--- Projet/index.php
+++ Projet/index.php
@@ -1,3 +1,15 @@
+<?php
+	
+	if ( (isset($_GET['langue'])) && ($_GET['langue'] == 'fr') )
+	{
+       	 	include_once('fr.php');
+    	}
+    	else // Langue par défaut (ici anglais)
+    	{
+        	include_once('en.php');
+    	}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -26,10 +38,10 @@
 		 		<li><a href="#" data-toggle="modal" data-target="#login-modal" style="font-size:20px;"> </a></li>
 		 	</ul>
 		 	<ul class="nav navbar-nav navbar-right">
-		 		<li><a href="#" data-toggle="modal" data-target="#login-modal" style="font-size:20px;"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+		 		<li><a href="#" data-toggle="modal" data-target="#login-modal" style="font-size:20px;"><span class="glyphicon glyphicon-log-in"></span> <?php echo $log1; ?></a></li>
 		     <li class="dropdown" style="margin-top:8px; margin-right:15px;">
 		        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-		        Select Language
+		        <?php echo $langue; ?>
 						<span class="caret"></span>
 						</button>
 						<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
@@ -45,10 +57,10 @@
 	<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: center;">
 		<div class="modal-dialog">
 		<div class="loginmodal-container">
-			<h1>Login to Your Account</h1><br>
+			<h1><?php echo $log2; ?></h1><br>
 				<form method="post" action="login.php">
-					<input type="text" name="username" placeholder="Username">
-					<input type="password" name="password" placeholder="Password">
+					<input type="text" name="username" placeholder="<?php echo $user; ?>">
+					<input type="password" name="password" placeholder="<?php echo $pass; ?>">
 					<input type="submit" name="login" class="login loginmodal-submit" value="Login">
 				 </form>
 		</div>
@@ -60,9 +72,9 @@
 		 <div class="col-sm-2 sidenav">
 		 </div>
 		 <div class="col-sm-8 text-left"> 
-		   <h1>Create your very own Agenda !</h1>
+		   <h1><?php echo $bienvenue; ?></h1>
 		   <br>
-		   <h2>In a few easy steps it'll look like this :</h2>
+		   <h2><?php echo $description1; ?></h2>
 		   <br>
 		   <h2>10h30 | Conférence 1</h2>
 		   <h4><span class="glyphicon glyphicon-bullhorn"></span> Conférence sur le jambon du WEI</h4>
